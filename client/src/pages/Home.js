b7c13f982b08ee2c712f77429b85216a0e34482a
ThiagoSFerraz <oniferraz@gmail.com>
2021-08-09 18:12:56 -0300

    New home with create post

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,55 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useQuery } from "@apollo/client";
-import gql from "graphql-tag";
 import { Grid } from "semantic-ui-react";
 
+import { AuthContext } from "../context/auth";
 import PostCard from "../components/PostCard";
+import PostForm from "../components/PostForm";
+
+import { FETCH_POSTS_QUERY } from "../util/graphql";
 
 function Home() {
+  const { user } = useContext(AuthContext);
+
   const { loading, data: { getPosts: posts } = {} } =
     useQuery(FETCH_POSTS_QUERY);
 
   return (
-    <Grid columns={2}>
-      <Grid.Row centered>
-        <h1>Recent Posts</h1>
-      </Grid.Row>
-      <Grid.Row>
-        {loading ? (
-          <h1>Loading posts...</h1>
-        ) : (
-          posts &&
-          posts.map((post) => (
-            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-              <PostCard post={post} />
+    <div>
+      <Grid columns={1}>
+        <Grid.Row centered>
+          <h1>Recent Posts</h1>
+        </Grid.Row>
+        <Grid.Row>
+          {user && (
+            <Grid.Column>
+              <PostForm />
             </Grid.Column>
-          ))
-        )}
-      </Grid.Row>
-    </Grid>
+          )}
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2}>
+        <Grid.Row>
+          {loading ? (
+            <h1>Loading posts...</h1>
+          ) : (
+            posts &&
+            posts.map((post) => (
+              <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))
+          )}
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 }
-
-const FETCH_POSTS_QUERY = gql`
-  {
-    getPosts {
-      id
-      body
-      createdAt
-      username
-      likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id
-        username
-        createdAt
-        body
-      }
-    }
-  }
-`;
 
 export default Home;
